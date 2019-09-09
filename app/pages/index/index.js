28a2e38711b0aaaa3d8a3d9d673bb0e1ad134a79
namelzx <914057262@qq.com>
2019-09-09 14:10:01 +0800

    日常更新

---
+++ app/pages/index/index.js
@@ -0,0 +1,76 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    inviter:'',
+    name:'',
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  // 输入邀请人
+  bindInviter(e){
+    this.setData({
+      inviter: e.detail.value
+    })
+  },
+  // 输入姓名
+  bindNameInput(e){
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  // 提交数据
+  handelPost(){
+    var that=this;
+    var temp={
+      inviter: that.data.inviter,
+      name: that.data.name,
+    }
+  }
+})
